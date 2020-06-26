fe2dd0990d421bbe59fa6bedf533bbb770842d90
Subrata Halder <subrata@sureify.com>
2020-06-26 20:03:42 +0530

    commit example

---
+++ src/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "log"
+import "net/http"
+import "github.com/gorilla/mux"
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/cars", func(rw http.ResponseWriter, r *http.Request) {
+		rw.Header().Set("Content-type", "application/json")
+		rw.WriteHeader(http.StatusOK)
+		rw.Write([]byte(`["BMW","Mercedes","Bentley","Audi","Cadillac"]`))
+	})
+
+	srv := &http.Server{
+		Handler: r,
+		Addr:    ":8080",
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
